49e19d34deb11708aac979f3f9fdd607bb53ebdf
MinminRepo <imthepapercat@gmail.com>
2023-09-28 14:20:48 +0800

    Registration tests.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,13 @@
 var express = require("express");
+const { type } = require("os");
 var router = express.Router();
+
+let mockUserList = [
+  {
+    userid: 2023000001,
+    data: "Lorem Ipsum",
+  },
+];
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -27,6 +35,50 @@ router.post("/login", async (req, res, next) => {
       message: "Login Unauthorized",
       sessionKey: null,
       userid: null,
+    });
+  }
+});
+
+router.post("/new", async (req, res, next) => {
+  let hasEmptyFlag = false;
+
+  let k = Object.keys(req.body);
+  k.forEach((key) => {
+    if (req.body[key] === "" || typeof req.body[key] === "undefined") {
+      /** missing values on object */
+      hasEmptyFlag = true;
+    }
+  });
+
+  if (hasEmptyFlag) {
+    res.status(422).json({
+      status: 422,
+      message: "Incomplete registration data",
+    });
+  } else {
+    res.status(201).json({
+      status: 201,
+      message: "Registration successful",
+      userid: 2023000001,
+    });
+  }
+});
+
+router.get("/info/:userid", async (req, res, next) => {
+  const matchingUsers = mockUserList.filter((user) => {
+    return user.userid === Number(req.params.userid);
+  });
+
+  if (matchingUsers.length !== 0) {
+    res.status(200).json({
+      status: 200,
+      message: "User data found",
+      data: matchingUsers,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      message: "Record with given key not found",
     });
   }
 });
--- tests/app.test.js
+++ tests/app.test.js
@@ -10,6 +10,66 @@ describe("Basic Setup: PNPACAT Online Application System REST API", () => {
 
   it("Can register as user", () => {
     /** auto-pass */
+    return request(app)
+      .post("/users/new")
+      .send({
+        lastname: "Dela Cruz",
+        givenname: "Juan",
+        middlename: "Cortez",
+        qualifier: "Jr.",
+        bdate: "2002-03-15",
+        email: "jcjrdelax@gmail.com",
+        password: "dxjuan0315",
+        address: {
+          home: "N.A.",
+          brgy: "01010101",
+          city: "010101",
+          prov: "0101",
+          region: "01",
+        },
+      })
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            status: 201,
+            message: expect.any(String),
+            userid: expect.any(Number),
+          })
+        );
+      });
+  });
+
+  it("Denies incomplete registration data", () => {
+    return request(app)
+      .post("/users/new")
+      .send({
+        lastname: "Dela Cruz",
+        givenname: "Juan",
+        middlename: "",
+        qualifier: "Jr.",
+        bdate: "2002-03-15",
+        email: null,
+        password: "dxjuan0315",
+        address: {
+          home: "N.A.",
+          brgy: "01010101",
+          city: "010101",
+          prov: "0101",
+          region: "01",
+        },
+      })
+      .expect(422)
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            status: 422,
+            message: expect.any(String),
+          })
+        );
+      });
   });
 
   it("Can login as user", () => {
@@ -54,5 +114,20 @@ describe("Basic Setup: PNPACAT Online Application System REST API", () => {
           })
         );
       });
+  });
+
+  it("Can get user information via user id", () => {
+    /** autopass */
+    return request(app)
+      .get("/users/info/2023000001")
+      .expect(200)
+      .expect("Content-Type", /json/);
+  });
+
+  it("Handles non-existent user ids", () => {
+    return request(app)
+      .get("/users/info/2023999999")
+      .expect(404)
+      .expect("Content-Type", /json/);
   });
 });
